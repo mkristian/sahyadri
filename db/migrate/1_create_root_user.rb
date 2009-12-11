253d61926d7ebac56e55990cb423165d8b0d6dde
mkristian <m.kristian@web.de>
2009-12-11 21:29:46 +0530

    new ixtlan models

--- db/migrate/1_create_root_user.rb
+++ db/migrate/1_create_root_user.rb
@@ -1,11 +1,12 @@
 migration 1, :create_root_user do
   up do
-    Ixtlan::Models::User.auto_migrate!
-    Ixtlan::Models::Locale.auto_migrate!
-    Ixtlan::Models::Group.auto_migrate!
+    User.auto_migrate!
+    Locale.auto_migrate!
+    Group.auto_migrate!
     Ixtlan::Models::GroupUser.auto_migrate!
+    Ixtlan::Models::GroupLocaleUser.auto_migrate!
 
-    u = Ixtlan::Models::User.new(:login => 'root', :email => 'root@exmple.com', :name => 'Superuser', :language => 'en', :id => 1)
+    u = User.new(:login => 'root', :email => 'root@exmple.com', :name => 'Superuser', :language => 'en', :id => 1)
     #u.current_user = u
     u.created_at = DateTime.now
     u.updated_at = u.created_at
@@ -13,7 +14,7 @@ migration 1, :create_root_user do
     u.updated_by_id = 1
     u.reset_password
     u.save!
-    g = Ixtlan::Models::Group.create(:name => 'root', :current_user => u)
+    g = Group.create(:name => 'root', :current_user => u)
     u.groups << g
     u.save
     STDERR.puts "#{u.login} #{u.password}"
--- db/migrate/2_create_configuration.rb
+++ db/migrate/2_create_configuration.rb
@@ -1,7 +1,7 @@
 migration 2, :create_configuration do
   up do
-    Ixtlan::Models::Configuration.auto_migrate!
-    Ixtlan::Models::Configuration.create(:session_idle_timeout => 10, :keep_audit_logs => 3, :current_user => Ixtlan::Models::User.first)
+    Configuration.auto_migrate!
+    Configuration.create(:session_idle_timeout => 10, :keep_audit_logs => 3, :current_user => User.first)
   end
 
   down do
