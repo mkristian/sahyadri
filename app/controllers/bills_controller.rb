59e5da8e5b44910708d385157c9836166a05e955
kristian <m.kristian@web.de>
2011-01-30 15:21:21 +0530

    added directory browsing

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -5,9 +5,15 @@ class BillsController < ApplicationController
 
   # GET /bills
   def index
+    @dir = params[:dir] if params[:dir]
     path = File.join(Rails.root, Configuration.instance.bills_directory, current_user.login)
+    path = File.join(path, @dir) if @dir
     if File.exists?(path) && File.directory?(path)
-      @files = Dir["#{path}/**"].collect { |f| File.basename(f) }
+      @files = Dir[File.join(path, "**")].collect do |f| 
+        file = File.basename(f)
+        file = File.join(file, "") if File.directory?(f)
+        file
+      end
     else
       render :text => "no files for #{current_user.login}"
     end
@@ -16,12 +22,14 @@ class BillsController < ApplicationController
   # GET /bills/file
   # GET /bills/1.xml
   def show
+    dir = params[:dir] if params[:dir]
     filename = if params[:ext]
                  params[:file] + "." + params[:ext]
                else
                  params[:file]
                end
-    path = File.join(Rails.root, 'bills', current_user.login, filename)
+    path = File.join(Rails.root, Configuration.instance.bills_directory, current_user.login)
+    path = dir ? File.join(path, dir, filename) : File.join(path, filename)
     if File.exists?(path)
       send_file(path)
     else
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ ActionController::Routing::Routes.draw do |map|
 
   map.connect 'bills', :controller => :bills, :action => 'index'
   
+  map.connect 'bills/:dir', :controller => :bills, :action => 'index'
+  
   map.connect 'bills/:file.:ext', :controller => :bills, :action => 'show'
+  
+  map.connect 'bills/:dir/:file.:ext', :controller => :bills, :action => 'show'
   
   map.resources :audits
 
@@ -12,21 +16,13 @@ ActionController::Routing::Routes.draw do |map|
 
   map.resources :locales
 
-  map.resources :locales
-
   map.resources :word_bundles
 
   map.resources :phrases
 
   map.resources :domains
 
-  map.resources :domains
-
   map.resources :groups
-
-  map.resources :groups
-
-  map.resources :users
 
   map.resources :users
 
